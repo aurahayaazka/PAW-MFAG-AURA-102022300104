e19e6d25189a28a226e34d0f9d4b79dd1a17ea58
aurahayaazka <myaura.haya.azka@gmail.com>
2025-05-03 15:24:15 +0700

    JURNAL_MODUL4

---
+++ JURNAL_MODUL_4_REG/app/Http/Controllers/CommentController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+     /**
+     * - Validasi input komentar ('comment' wajib diisi dan maksimal 1000 karakter)
+     * - Simpan komentar baru pada artikel tertentu, relasikan dengan user yang sedang login
+     * - Tampilkan pesan sukses dan redirect ke halaman detail artikel
+     */
+    public function store(Request $request, Article $article)
+    {
+        $request->validate([
+            'title' =>'required',
+            'content' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,gif,svg|max:2048',
+        ]);
+
+        $articleData = $request->only('title', 'content');
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('articles', 'public');
+            $articleData['image'] = $imagePath;
+        }
+
+        auth()->user()->articles()->create($articleData);
+
+        session()->flash('success', 'Artikel berhasil dibuat!');
+
+        return redirect()->route('admin.index');
+    }
+
+    public function destroy(Comment $comment) {
+        $articleId = $comment->article_id;
+        $comment->delete();
+    
+        session()->flash('success', 'Comment berhasil dihapus!');
+        return redirect()->route('articles.show', ['article' => $articleId]);
+    }
+}
